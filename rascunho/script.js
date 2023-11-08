4506845ae272e75b482740547f99d880c072db64
frnadin <127996959+frnadin@users.noreply.github.com>
2023-11-07 21:30:52 -0300

    Add files via upload
    adicionando biblioteca jQuerry

--- rascunho/script.js
+++ rascunho/script.js
@@ -22,6 +22,7 @@ document.getElementById('log').style.textDecorationLine = "none"
 }
 
 function login(){ // função para aparecer o login
+    $("#login").fadeIn(); 
     document.getElementById('login').style.display = "block"
     }
 
@@ -34,27 +35,29 @@ const valueSoda = 4.99
 const valueSauce = 0.99
 
 let qtdPizza = 0
-let totalPizza = 0
+let totalPizza = "-"
 
 let qtdBurguer = 0
-let totalBurguer = 0 
+let totalBurguer = "-"
 
 let qtdDog = 0
-let totalDog = 0
+let totalDog =  "-"
 
 let qtdPotato = 0
-let totalPotato = 0
+let totalPotato =  "-"
 
 let qtdSoda = 0
-let totalSoda = 0 
+let totalSoda =  "-" 
 
 let qtdSauce = 0 
-let totalSauce = 0
+let totalSauce =  "-"
 
 function pizza(){
  qtdPizza = document.getElementById("qtdPiz").value
  totalPizza = qtdPizza*valuePizza
 console.log(qtdPizza)
+
+notifacaoCompra()
 }
 
 function burguer(){
@@ -62,30 +65,49 @@ qtdBurguer = document.getElementById("qtdBur").value
 totalBurguer = qtdBurguer*valueBurguer
 console.log(qtdBurguer)
 
+notifacaoCompra()
 }
 
 function dog(){
  qtdDog =  document.getElementById("qtdDog").value
  totalDog = qtdDog*valueDog
 console.log(qtdDog)
+
+notifacaoCompra()
 }
 
 function potato(){
 qtdPotato =  document.getElementById("qtdPot").value
 totalPotato = qtdPotato*valuePotato
 console.log(qtdPotato)
+
+notifacaoCompra()
 }
 
 function soda(){
 qtdSoda =  document.getElementById("qtdRef").value
 totalSoda = qtdSoda*valueSoda
 console.log(qtdSoda)
+
+notifacaoCompra()
 }
 
 function sauce(){
 qtdSauce = document.getElementById("qtdSau").value
 totalSauce = qtdSauce*valueSauce
 console.log(qtdSauce)
+
+notifacaoCompra()
+}
+
+let totalCompra = 0
+
+function fechamentoCompra(value){
+    if(value!="-"){
+ totalCompra += value 
+    }else{
+        // console.log("error")
+    }
 }
 
 function finalizar(){
@@ -94,19 +116,52 @@ function finalizar(){
 
   document.getElementById('saidaQtdPiz').textContent = qtdPizza 
   document.getElementById('saidaPiz').textContent = totalPizza 
+  fechamentoCompra(totalPizza)
 
   document.getElementById('saidaQtdBur').textContent = qtdBurguer 
   document.getElementById('saidaBur').textContent = totalBurguer
+  fechamentoCompra(totalBurguer)
 
   document.getElementById('saidaQtdDog').textContent = qtdDog
   document.getElementById('saidaDog').textContent = totalDog 
+  fechamentoCompra(totalDog)
 
   document.getElementById('saidaQtdPot').textContent = qtdPotato 
   document.getElementById('saidaPot').textContent = totalPotato
+  fechamentoCompra(totalPotato)
 
   document.getElementById('saidaQtdSof').textContent = qtdSoda 
   document.getElementById('saidaSof').textContent = totalSoda 
-  
+  fechamentoCompra(totalSoda)
+
   document.getElementById('saidaQtdSau').textContent = qtdSauce 
   document.getElementById('saidaSau').textContent = totalSauce
+  fechamentoCompra(totalSauce)
+
+  document.getElementById('totalProd').textContent =  totalCompra 
+
 }
+
+// document.getElementsByClassName("btnBuy").addEventListener("click", notifacaoCompra)
+
+function notifacaoCompra(){ 
+
+    var notificacao = document.querySelector('#notificaoCompra');
+    var styles = window.getComputedStyle(notificacao);
+    var display = styles.getPropertyValue("display")
+
+   console.log(display)
+
+ if(display == "none"){
+    $("#notificaoCompra").fadeIn(); 
+    console.log("colocou ")
+  
+}
+
+function tiraNotificação(){
+    $("#notificaoCompra").fadeOut();
+    console.log("tirou ")
+}
+setTimeout(tiraNotificação, 1000)
+}
+
